19a419fb073fe58019b2b7e0775f04a6710d9711
58Dmitriy <dimitri_e@mail.ru>
2025-04-26 21:49:19 +0300

    Add 2-branch

---
+++ Zanatie.py
@@ -0,0 +1,57 @@
+# Ввод значения от пользователя и преобразование в число
+# b = int(input('Введи 2 число: '))
+# a = int(input('Введи 1 число: '))
+# print("a = ", a) # Вывод переменной a
+# print("a = ", b) # Вывод переменной b
+# print("Сумма: ", a+b) # Вывод суммы двух переменных (a+b)
+#
+# """
+# Третье занятие
+# по Python
+# """
+#
+# a = 5
+# b = 0
+# c = -10
+# d = 500000
+# e = 600_000_000
+# print(a, b, c, d, e)
+#
+# Дробные числа
+#
+# a = 3.5
+# b = 0.0
+# c = -5.6
+# d = 500_000.000_003
+# e = 4.5e2
+# print(a, b, c, d, e)
+#
+# # Логический тип данных
+# print(bool(0))
+# print(bool(-5))
+# print(bool("Привет мир"))
+# print(bool(""))
+# print(bool(" "))
+# print(bool(False))
+# print(bool(True))
+# Сравнения
+
+# print(2 == "2")
+# print(4 < 5)
+# print(5 <= 5)
+# print(5 >= 5)
+# print(10 >= 5)
+# print(10 <= 5)
+# print(1 < 5 < 10)
+#
+# x = 5
+# a = 1
+# b = 10
+# print(a > x < b)
+
+# Списки (list)
+my_List = ["Строка", 57, 5.5, True]
+print(my_List)
+print(my_List[3])
+print(my_List[2:4])
+print(my_List[2:])
